1a2a8ccfc28c30a06a9a8119f0c6928aa3807a4d
Joe Philip <joe.philip@hotmail.co.in>
2024-01-22 22:08:57 +0530

    defined url for login view

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 from . import views
 
 urlpatterns = [
-    path('signup', views.SignupAPI.as_view())
+    path('signup', views.SignupAPI.as_view()),
+    path('login', TokenObtainPairView.as_view(), name='login')
 ]
