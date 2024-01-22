4dc82c2dce9bf99a5fe5c4366d6a12f6dc40b604
Joe Philip <joe.philip@hotmail.co.in>
2024-01-22 22:57:18 +0530

    defined TextSnippetsAPI

--- main/views.py
+++ main/views.py
@@ -1,9 +1,21 @@
+from django.db.models import QuerySet
 from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
 
-from .serializers import UserSignupSerializer
+from .models import TextSnippets
+from .serializers import TextSnippetsSerializer, UserSignupSerializer
 
 # Create your views here.
 
 
 class SignupAPI(CreateAPIView):
     serializer_class = UserSignupSerializer
+
+
+class TextSnippetsAPI(ModelViewSet):
+    permission_classes = (IsAuthenticated,)
+    serializer_class = TextSnippetsSerializer
+
+    def get_queryset(self) -> QuerySet[TextSnippets]:
+        return TextSnippets.objects.filter(created_user=self.request.user)
