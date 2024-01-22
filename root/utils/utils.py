61077e0a9ba5dc7e664e317de1ddcdb413f0e50f
Joe Philip <joe.philip@hotmail.co.in>
2024-01-22 22:55:46 +0530

    defined slug_generate()

--- root/utils/utils.py
+++ root/utils/utils.py
@@ -1,4 +1,8 @@
+from datetime import datetime
 from typing import Any
+
+from django.utils.text import slugify
+from pytz import timezone
 
 
 def success(data: Any = None) -> dict:
@@ -7,3 +11,7 @@ def success(data: Any = None) -> dict:
 
 def fail(error) -> dict:
     return {'status': False, 'message': 'fail', 'error': error}
+
+
+def slug_generate(key: str = 'slug') -> str:
+    return slugify(f'{key}-{datetime.now(tz=timezone("Asia/Calcutta"))}')
