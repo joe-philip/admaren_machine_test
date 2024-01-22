ceef83de208f9241b2b64ba717f71eaf7f54c938
Joe Philip <joe.philip@hotmail.co.in>
2024-01-22 21:35:40 +0530

    defined UserSignupSerializer

--- main/serializers.py
+++ main/serializers.py
@@ -1,1 +1,22 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
+
+
+class UserSignupSerializer(serializers.ModelSerializer):
+    def validate_email(self, value: str) -> str:
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError('Email already exists')
+        return value
+
+    class Meta:
+        model = User
+        exclude = ('is_staff', 'is_active', 'date_joined')
+        extra_kwargs = {
+            'password': {'write_only': True},
+            'first_name': {'required': True},
+            'email': {'required': True}
+        }
+
+    def save(self, **kwargs) -> User:
+        self.instance = User.objects.create_user(**self.validated_data)
+        return self.instance
