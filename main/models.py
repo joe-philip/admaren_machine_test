b2e51f504b43d43826865e1fa97237c91af0f02b
Joe Philip <joe.philip@hotmail.co.in>
2024-01-22 21:37:15 +0530

    defined TextSnippets model

--- main/models.py
+++ main/models.py
@@ -11,3 +11,18 @@ class Tags(models.Model):
         verbose_name = 'Tag'
 
     def __str__(self) -> str: return self.title
+
+
+class TextSnippets(models.Model):
+    slug = models.SlugField(unique=True)
+    title = models.CharField(max_length=50)
+    snippet = models.TextField()
+    tags = models.ManyToManyField(Tags)
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'text_snippets'
+        verbose_name = 'Text snippet'
+
+    def __str__(self) -> str: return self.title
