94087df49d53a30efe49fed921f756f5ab0f0a78
Joe Philip <joe.philip@hotmail.co.in>
2024-01-22 21:36:55 +0530

    defined Tags model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Tags(models.Model):
+    title = models.CharField(max_length=50, unique=True)
+
+    class Meta:
+        db_table = 'tags'
+        verbose_name = 'Tag'
+
+    def __str__(self) -> str: return self.title
