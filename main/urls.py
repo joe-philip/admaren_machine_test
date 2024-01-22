e81910911f7e0bae1d17521619fe730115129bee
Joe Philip <joe.philip@hotmail.co.in>
2024-01-22 22:57:30 +0530

    defined URL for TextSnippetsAPI

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
+from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 from . import views
 
+router = DefaultRouter()
+
+router.register(
+    'text_snippets', views.TextSnippetsAPI,
+    basename='text_snippets'
+)
+
 urlpatterns = [
     path('signup', views.SignupAPI.as_view()),
     path('login', TokenObtainPairView.as_view(), name='login')
-]
+]+router.urls
