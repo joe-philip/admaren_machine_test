747197bfa80bfee3b4f055dea98bf35a7323a286
Joe Philip <joe.philip@hotmail.co.in>
2024-01-29 17:30:21 +0530

    defined TextSnippetsDeleteAPI

--- main/views.py
+++ main/views.py
@@ -1,8 +1,10 @@
 from django.db.models import QuerySet
+from rest_framework.exceptions import NotFound
 from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.request import Request
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 
 from .models import Tags, TextSnippets
@@ -35,6 +37,27 @@ class TextSnippetsAPI(ModelViewSet):
         self.perform_destroy(self.get_object())
         queryset = self.get_queryset()
         return Response(self.serializer_class(queryset, context=self.get_serializer_context(), many=True).data)
+
+
+class TextSnippetsDeleteAPI(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request: Request) -> Response:
+        from .serializers import TextSnippetsDeleteSerializer
+
+        serializer = TextSnippetsDeleteSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        snippets = map(
+            lambda x: x.id, serializer.validated_data.get('snippets', [])
+        )
+        queryset = TextSnippets.objects.filter(
+            created_user=self.request.user,
+        )
+        delete_queryset, response_queryset = queryset.filter(id__in=snippets), queryset.exclude(id__in=snippets)  # noqa E501 # pylint: disable=line-too-long
+        if delete_queryset.exists():
+            delete_queryset.delete()
+            return Response(TextSnippetsSerializer(response_queryset, context={'request': request}, many=True).data)
+        raise NotFound('No snippets found')
 
 
 class TagListAPI(ListAPIView):
